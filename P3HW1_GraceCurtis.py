1403e5756392e8bbe19f8e528fbd7bd14f1e10be
gracec4131 <99841059+gracec4131@users.noreply.github.com>
2022-05-08 18:02:45 -0400

    Add files via upload

---
+++ P3HW1_GraceCurtis.py
@@ -0,0 +1,6 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\Curtis\Downloads\Downloads\P3HW1_Debugging_GraceCurtis.py =
+Enter grade: 82
+Your grade is: B
